b7194f60a089bdfa07896eebeb5092dd3c350248
Cody St Pierre <codyslimshady9@gmail.com>
2026-02-02 12:09:27 -0600

    comments

--- redirect.js
+++ redirect.js
@@ -2,8 +2,6 @@ chrome.webNavigation.onCommitted.addListener((details) => {
     if (details.frameId !== 0) return; // Only main frame
 
     const url = new URL(details.url);
-
-    // Only act on Bing search pages
     if (url.hostname !== "www.bing.com" || url.pathname !== "/search") return;
 
     const q = url.searchParams.get("q");
@@ -11,6 +9,5 @@ chrome.webNavigation.onCommitted.addListener((details) => {
 
     const googleURL = "https://www.google.com/search?q=" + encodeURIComponent(q);
 
-    // Redirect
     chrome.tabs.update(details.tabId, { url: googleURL });
 });
